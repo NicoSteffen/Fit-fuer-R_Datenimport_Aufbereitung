42dd0ea9a5760952da87637df65af922f63ec482
NicoSteffen <nico@die-steffens.de>
2025-11-05 19:29:41 +0100

    Update

--- Fit_fuer_r.R
+++ Fit_fuer_r.R
@@ -1,55 +1,173 @@
+# install.packages("haven")
+#install.packages("openxlsx")
+
 library(haven)
 library(openxlsx)
+library(dplyr)
+library(psych)
+library(report)
+library(flextable)
 
-getwd()
-setwd("/Users/nicosteffen/Documents/01 Arbeit/01 CFH/06 Lehre/Fit für R/Fit-fuer-R_Datenimport_Aufbereitung")
-
+#getwd()
+#setwd("/Users/nicosteffen/Documents/01 Arbeit/01 CFH/06 Lehre/Fit für R/Fit-fuer-R_Datenimport_Aufbereitung")
 
 df = read.csv("klinische_patientendaten.csv")
 df = read.csv2("klinische_patientendaten.csv2")
 
-df = read_sav("klinische_patientendaten.sav")
+df_sav = read_sav("klinische_patientendaten.sav")
 
-df = read.xlsx("klinische_patientendaten.xlsx")
+df_xl = read.xlsx("klinische_patientendaten.xlsx")
 
-df = read_sav("https://raw.githubusercontent.com/NicoSteffen/Fit-fuer-R_Datenimport_Aufbereitung/main/klinische_patientendaten.sav")
-
-
+df = read.csv2("https://raw.githubusercontent.com/NicoSteffen/Fit-fuer-R_Datenimport_Aufbereitung/main/klinische_patientendaten.csv2")
 
 View(df)
 head(df)
 names(df)
-
-df$Patienten_ID
-df$Alter
-
-df[1,]
-df[,3]
-df[10,3]
-
 str(df)
+
+
+df[1:10,4]
+
+df$Alter[4]
+
+# Char in Faktor Variablen umwandeln 
 
 df$Geschlecht = as.factor(df$Geschlecht)
 df$Bildungsgrad = as.factor(df$Bildungsgrad)
 df$Behandlungsgruppe = as.factor(df$Behandlungsgruppe)
 df$Raucher = as.factor(df$Raucher)
 
+# basics 
+
+mean(df$Alter)
+
+mean(df$Alter[df$Behandlungsgruppe == "Intervention A"])
+mean(df[df$Behandlungsgruppe == "Intervention A",]$Alter)
+
+sd(df$Alter)
+var(df$Alter)
+
+table(df$Geschlecht)
+table(df$Bildungsgrad)
+
+
+# grep
+
+df[,grep("LQ_", names(df))]
+
+select(df, starts_with("LQ_"))
+
+
+
+# Daten Aufbereiten 
+
+# ID1 Testlauf
+
+df = df[df$Patienten_ID != "PAT_1001",]
 
 table(df$Geschlecht)
 
-mean(df$Alter)
+df = df[df$Geschlecht != "Divers",]
+
+table(df$Geschlecht)
+
+df$Geschlecht = droplevels(df$Geschlecht)
+
+# Alter < 75
+
+df = df[df$Alter < 76,]
+
+range(df$Alter)
+
+
+df_dplyr = filter(df,
+                  Patienten_ID != "PAT_1001",
+                  Alter < 76,
+                  Geschlecht != "Divers")
+
 mean(df$BMI, na.rm = T)
 
-# Zeilen auswählen 
+# Datenaggregation, Umkodieren
 
-df[3,]
-df[6:8,]
+# LQ_1 invertiert 
 
-df[c(2,4,7),]
+df$LQ_01 = 6 - df$LQ_01
 
-mean(df[df$Behandlungsgruppe == "Intervention A",]$BMI, na.rm = T)
+df$LQ_01 = dplyr::recode(df$LQ_01, `1` = 5, `2` = 3, `4` = 2, `5` = 1)
 
-mean(df$BMI[df$Geschlecht == "Weiblich"], na.rm=T)
+# Skalen aggregieren 
+
+df$LQ_total = rowMeans(df[,grep("LQ_", names(df))], na.rm = T)
+
+# STR -> Summe 
+
+colSums(is.na(df[,grep("STR", names(df))]))
+
+df$STR_sum = rowSums(df[,grep("STR", names(df))])
+
+
+write.csv2(df, "df_sauber.csv2")
+saveRDS(df, "df_sauber.rds")
+
+# Deskreptivstatistiken 
+
+
+psych::describe(df)
+describeBy(df, group = df$Behandlungsgruppe)
+
+report_sample(df, by = "Behandlungsgruppe")
+
+sample_report = df[,c("Alter", "Geschlecht", "Bildungsgrad", "Behandlungsgruppe", "BMI", "Raucher", "LQ_total", "STR_sum" )]
+
+report_sample(sample_report, by = "Behandlungsgruppe")
+
+flextable(report_sample(sample_report, by = "Behandlungsgruppe"))
+
+flextable::save_as_docx(flextable(report_sample(sample_report, by = "Behandlungsgruppe")), path = "Table1.docx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
